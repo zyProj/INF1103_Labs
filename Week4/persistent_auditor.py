80be7781c8800cfc950da078df9a8e0ce02e225f
ZhaoYan <2601449@sit.singaporetech.edu.sg>
2026-09-25 11:43:26 +0800

    updated load_inventory() with exception handling

--- Week4/persistent_auditor.py
+++ Week4/persistent_auditor.py
@@ -1,6 +1,13 @@
 def load_inventory():
-    with open("Week4/inventory.txt", "r") as file:
-        inventory = file.readlines()
-    return inventory
+    try:
+        nested_list = []
+        with open("Week4/inventory.txt", "r") as file:
+            for line in file:
+                row = line.strip().split(",")
+                nested_list.append(row)
+        return nested_list
+
+    except FileNotFoundError:
+        return []
 
 print(load_inventory())
